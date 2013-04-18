86045ede8b23722b1ab63e58cf80f5ef7b40d489
Francois Berenger <berenger@riken.jp>
2013-04-18 16:35:03 +0900

    almost ready to process the input files

--- setop.ml
+++ setop.ml
@@ -8,23 +8,23 @@ open Printf
 
 type set_op = Union | Inter | Diff
 
-module L  = BatList
 module F  = BatFile
+module HT = Hashtbl
+module L  = BatList
 module S  = BatString
 module SS = Set.Make (String)
 
 type all_options =
-    { f1   : string ref ; (* first input file *)
-      f2   : string ref ; (* second input file *)
-      op   : string ref ; (* set operation *)
-      sep1 : char ref   ; (* field separator in f1
-                             FBR: maybe a string ref later on *)
-      sep2 : char ref   ; (* field separator in f2 *)
-      osep : char ref   ; (* field separator in output file *)
-      out  : string ref ; (* output file *)
-      col1 : int ref    ; (* key field number in f1 *)
-      col2 : int ref    ; (* key field number in f2 *)
+    { f1   : string ref ;
+      f2   : string ref ;
+      op   : string ref ;
+      out  : string ref ;
       debug: bool ref   }
+
+let process_file f sepchar colnum =
+  let ht = HT.create 1000 in
+  let set = SS.empty in
+  ht, set
 
 (* main *)
 
@@ -33,12 +33,7 @@ let opts = {
   f1 = ref "";
   f2 = ref "";
   op = ref "";
-  sep1 = ref ' ';
-  sep2 = ref ' ';
-  osep = ref ' ';
-  out = ref "/dev/stdout";
-  col1 = ref 1;
-  col2 = ref 1;
+  out = ref "/dev/stdout:' '";
   debug = ref false} in
 let cmd_line = Arg.align [
   "-f1", Arg.Set_string opts.f1   , "file1:'sepchar':colnum";
@@ -48,18 +43,27 @@ let cmd_line = Arg.align [
                                      m1: set2 - set1"       ;
   "-o ", Arg.Set_string opts.out  , "output:'sepchar'"      ;
   "-v" , Arg.Set        opts.debug, " debug mode"           ] in
-let use_msg = sprintf
-  "usage: %s -db fragments_file -q query_frag -d 0.5 -n 10 -o out \
-             -idx fragments_RMSD_index [-x exclude_file] [-f] [-v] \
-                                       [-m {t2t|b2t|b2b}]"
-  Sys.argv.(0) in
+let use_msg = sprintf "usage: %s FBR: write this" Sys.argv.(0) in
 Arg.parse cmd_line ignore use_msg;
 
+if !(opts.f1) = "" then failwith "-f1 is mandatory";
+if !(opts.f2) = "" then failwith "-f2 is mandatory";
+if !(opts.op) = "" then failwith "-op is mandatory";
+
+(* extract sep. chars and col. numbers *)
+let f1, sep1, col1 =
+  Scanf.sscanf !(opts.f1) "%s:%c:%d" (fun f sep col -> f, sep, col) in
+let f2, sep2, col2 =
+  Scanf.sscanf !(opts.f2) "%s:%c:%d" (fun f sep col -> f, sep, col) in
+
 (* create the 1st key set and hash table *)
+let set1, ht1 = process_file f1 sep1 col1 in
 
 (* create the 2nd key set and hash table *)
+let set2, ht2 = process_file f2 sep2 col2 in
 
 (* operate on the key sets *)
 
 (* create and output concatenated lines *)
-()
+eprintf "The END\n";
+exit 0
