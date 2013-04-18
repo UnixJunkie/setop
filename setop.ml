e12ca17156d21a5fc9d3a1416e6f83486aedefb1
Francois Berenger <berenger@riken.jp>
2013-04-18 14:59:52 +0900

    added record to hold all options

--- setop.ml
+++ setop.ml
@@ -11,8 +11,24 @@ type set_op = Union | Inter | Diff
 module L = BatList
 module F = BatFile
 module S = BatString
+(* module Set = StringSet *)
+
+type all_options =
+    { in1  : string ref ; (* first input file *)
+      in2  : string ref ; (* second input file *)
+      op   : string ref ; (* set operator *)
+      sep1 : char ref   ; (* field separator in f1
+                             FBR: maybe a string ref later on *)
+      sep2 : char ref   ; (* field separator in f2 *)
+      osep : char ref   ; (* field separator in output file *)
+      out  : string ref ; (* output file *)
+      col1 : int ref    ; (* key field number in f1 *)
+      col2 : int ref    ; (* key field number in f2 *)
+      debug: bool ref   }
 
 (* main *)
+
+(* process options *)
 
 (* create the 1st key set and hash table *)
 
